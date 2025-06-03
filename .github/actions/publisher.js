4be4160394573946bff32bca2cdd1775a0b77132
Darin Kuntze <dkuntze@gmail.com>
2025-06-03 16:46:27 -0500

    change path

--- .github/actions/publisher.js
+++ .github/actions/publisher.js
@@ -1,3 +1,3 @@
 console.log('DA_TOKEN:', process.env.DA_TOKEN);
 console.log('HELIX_TOKEN:', process.env.HELIX_TOKEN);
-console.log('CLIENT_PAYLOAD_PATH:', process.env.CLIENT_PAYLOAD_PATH); 
+console.log('CLIENT_PAYLOAD_PATH:', github.context.payload.client_payload.path); 
