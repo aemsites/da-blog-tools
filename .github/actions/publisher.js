d9fe18825be10efcb0bb46a88c93036555e85a06
Darin Kuntze <dkuntze@gmail.com>
2025-06-03 17:01:53 -0500

    rudimentary path detection

--- .github/actions/publisher.js
+++ .github/actions/publisher.js
@@ -1,4 +1,18 @@
-console.log('DA_TOKEN:', process.env.DA_TOKEN);
-console.log('HELIX_TOKEN:', process.env.HELIX_TOKEN);
-console.log('CLIENT_PAYLOAD_PATH:', process.env['AEM_PAGE_PATH']);
-console.log(process.env); 
+const DA_TOKEN = process.env.DA_TOKEN;
+const HELIX_TOKEN = process.env.HELIX_TOKEN;
+const AEM_PAGE_PATH = process.env.AEM_PAGE_PATH;
+
+function main() {
+  console.log('DA_TOKEN:', DA_TOKEN);
+  console.log('HELIX_TOKEN:', HELIX_TOKEN);
+  console.log('AEM_PAGE_PATH:', AEM_PAGE_PATH);
+  // console.log(process.env); // uncomment out if you want to see all the env variables
+
+  if (AEM_PAGE_PATH && AEM_PAGE_PATH.startsWith('/drafts/')) {
+    console.log('AEM_PAGE_PATH starts with /drafts/');
+  } else {
+    console.log('AEM_PAGE_PATH does not start with /drafts/');
+  }
+}
+
+main();
