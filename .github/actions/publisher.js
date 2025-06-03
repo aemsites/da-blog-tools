ec93634e5b0f53eb131297f17ce729c79de536bf
Darin Kuntze <dkuntze@gmail.com>
2025-06-03 16:42:56 -0500

    more tests

--- .github/actions/publisher.js
+++ .github/actions/publisher.js
@@ -1,2 +1,3 @@
 console.log('DA_TOKEN:', process.env.DA_TOKEN);
-console.log('HELIX_TOKEN:', process.env.HELIX_TOKEN); 
+console.log('HELIX_TOKEN:', process.env.HELIX_TOKEN);
+console.log('CLIENT_PAYLOAD_PATH:', process.env.CLIENT_PAYLOAD_PATH); 
